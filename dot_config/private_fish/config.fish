013ea8676c801e7aaab1a8429e8b4ebdbbcc68ba
Truxnell <nat@voltaicforge.com>
2022-01-22 08:50:54 +1100

    feat: add more environments

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,12 +1,24 @@
+# Disable fish greeting
+set -g fish_greeting
+
+# Set Editor
 set -gx KUBE_EDITOR nano
 set -gx VISUAL nano
 set -gx EDITOR nano
+
+# Set SOPS age
 set -gx SOPS_AGE_KEY_FILE {{ .chezmoi.homeDir }}/.config/sops/age/keys.txt
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Enable thefuck
 if type -q thefuck
     thefuck --alias | source
+end
+
+## Source custom configs
+for file in $__fish_config_dir/custom.d/*.fish
+    source $file
 end
