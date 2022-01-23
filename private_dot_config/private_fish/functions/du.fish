623ba2d685d3cbb6e17e2e3a118c9f88ff7f591f
Nat Allan <nat@natallan.com>
2022-01-23 19:25:41 +1100

    fix: made function folder private

--- private_dot_config/private_fish/functions/du.fish
+++
@@ -1,8 +0,0 @@
-# Largely ripped from https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/common-aliases/common-aliases.plugin.zsh
-function dud --description 'size of directories in this folder'
-    du -d 1 -h
-end
-
-function duf --description 'size of files and dirs in this folder'
-    du -sh *
-end
