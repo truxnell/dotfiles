4e15cdb3f942d19d0e852013d68be2b83f540944
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2022-11-30 21:51:34 +1100

    feat: add wsl syncthing

--- dot_config/private_fish/private_functions/private_pacman.fish
+++
@@ -1,8 +0,0 @@
-function yeet --description 'removes package'
-    if count $argv > /dev/null
-        {$AUR_HELPER} -Rns $argv
-    else
-        echo "Error - Argument needed (Usage: yeet packagename)"
-    end
-end
-    
