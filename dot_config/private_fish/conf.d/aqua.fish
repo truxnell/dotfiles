16587a87845febb447fbbae7e0db12b6a907a82a
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-05-14 21:39:58 +1000

    fix: fix aqua path

--- dot_config/private_fish/conf.d/aqua.fish
+++ dot_config/private_fish/conf.d/aqua.fish
@@ -1,4 +1,4 @@
 set -gx AQUA_CONFIG $HOME/.config/aqua/aqua.yaml
 set -gx AQUA_GLOBAL_CONFIG $HOME/.config/aqua/aqua.yaml
-set -gx AQUA_ROOT_DIR $HOME/.local/share/aqua
+set -gx AQUA_ROOT_DIR $HOME/.local/share/aquaproj-aqua/
 fish_add_path $AQUA_ROOT_DIR/bin
