61f9f957e8dc5155f3312a81930f40758106629f
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-05-20 22:27:35 +1000

    Update .config/nvim/lua/plugins/tools.lua

--- dot_config/nvim/lua/plugins/tools.lua
+++ dot_config/nvim/lua/plugins/tools.lua
@@ -3,25 +3,25 @@ return {
   { "pwntester/octo.nvim", opts = {}, cmd = "Octo" },
 
   -- markdown preview
-  {
-    "toppair/peek.nvim",
-    build = "deno task --quiet build:fast",
-    keys = {
-      {
-        "<leader>op",
-        function()
-          local peek = require("peek")
-          if peek.is_open() then
-            peek.close()
-          else
-            peek.open()
-          end
-        end,
-        desc = "Peek (Markdown Preview)",
-      },
-    },
-    opts = { theme = "light" },
-  },
+  -- {
+  --   "toppair/peek.nvim",
+  --   build = "deno task --quiet build:fast",
+  --   keys = {
+  --     {
+  --       "<leader>op",
+  --       function()
+  --         local peek = require("peek")
+  --         if peek.is_open() then
+  --           peek.close()
+  --         else
+  --           peek.open()
+  --         end
+  --       end,
+  --       desc = "Peek (Markdown Preview)",
+  --     },
+  --   },
+  --   opts = { theme = "light" },
+  -- },
 
   -- better diffing
   {
